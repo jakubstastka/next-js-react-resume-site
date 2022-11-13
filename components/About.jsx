0e6f14b6477ffb3eeddbc47ab00fba749a8045f8
Jakub Stastka <jakub@stastka.xyz>
2022-11-13 16:47:03 +0100

    About text edited and added export script

--- components/About.jsx
+++ components/About.jsx
@@ -10,16 +10,26 @@ export default function About() {
           </p>
           <h2 className="py-2 text-gray-600">Who am I?</h2>
           <p className="py-2 text-gray-600">
-            I spent over 11 years working as a journalist. Since I have been
-            always interested in technology and coding, it was only a matter of
-            time before I started developing my own tools in Python to help me
-            with my day job.
+            I am not what you might call a typical developer. I spent over 11
+            years working as a journalist. Since I have been always interested
+            in technology and coding, it was only a matter of time before I
+            started developing my own tools in Python to help me with my day
+            job.
           </p>
           <p className="py-2 text-gray-600">
-            I switched carrers to software engineering, specializing in web
-            application development. I work with Python web framework Django,
-            HTML, CSS (Bootstrap, Tailwind), and TypeScript/JavaScript (React,
-            jQuery).
+            When the Covid-19 pandemic began, I took it upon myself to create a
+            thing that would write articles on its own. The data source was
+            already existing, so it was only a matter of integrating API and
+            make a script to generate the text. The project grew and grew,
+            eventually getting the name E-Gon (after the famous journalist Egon
+            Erwin Kitch) and becoming a Django web app.
+          </p>
+          <p className="py-2 text-gray-600">
+            Realizing that being a developer is much more fun than being a
+            journalist, I switched carrers to software engineering, specializing
+            in web application development. I work with Python web framework
+            Django, HTML, CSS (Bootstrap, Tailwind), and TypeScript/JavaScript
+            (React, jQuery).
           </p>
 
           <p className="py-2 text-gray-600">
@@ -29,10 +39,8 @@ export default function About() {
 
           <p className="py-2 text-gray-600">
             In my spare time, I like to learn new things about software
-            development, which includes some personal projects. One of which is
-            E-Gon, an electronic journalist. Its main task is to automate
-            production of news articles. The current project is to provide
-            automated news about the Covid-19 situation in the Czech Republic.
+            development, which includes some personal projects. I list some of
+            those projects with their git repos in the Projects section.
           </p>
         </div>
         <div>
